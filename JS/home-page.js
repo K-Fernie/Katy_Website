9c9a2019efeff808a39f951f53ebe8f12095ae41
kfernelius <kkin001@gmail.com>
2022-07-08 16:57:04 -0500

    Got Card flipping working

---
+++ JS/home-page.js
@@ -0,0 +1,17 @@
+const deck4 = document.querySelector(".fourth");
+
+deck4.addEventListener("click", function (e) {
+  deck4.classList.toggle('is-flipped');
+});
+
+const deck3 = document.querySelector(".third");
+
+deck3.addEventListener("click", function (e) {
+    deck3.classList.toggle('is-flipped');
+  });
+
+const deck2 = document.querySelector(".second");
+
+deck2.addEventListener("click", function (e) {
+    deck2.classList.toggle('is-flipped');
+  });
